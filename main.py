c64c868c06e936457f62f6323c9355f0ab346e77
Nazar Yakubiv <nyakubiv@uwo.ca>
2024-04-05 21:06:28 -0400

    anti semitism test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+masya= "masya"
+nazar = "nazar"
+#create anti semitism test
+def antiSemitismTest(name) : 
+    if (name == masya): 
+        print(f"{name} really hates jewish people. \nFAIL")
+    else: 
+        print(f"{name} really likes the jewish community. \nPASS")      
 
+#demo 
+
+antiSemitismTest(masya)
